c24c549c34674666fbc9c5fc37a34445c8c0934b
Aditya Kedar Tiwari <aditakedart@Adityas-MacBook-Pro.local>
2023-01-16 18:47:00 +0530

    App Setup

---
+++ esop_trading/src/main/kotlin/esop_trading/Application.kt
@@ -0,0 +1,7 @@
+package esop_trading
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ esop_trading/src/test/kotlin/esop_trading/Esop_tradingTest.kt
@@ -0,0 +1,19 @@
+package esop_trading
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class Esop_tradingTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
