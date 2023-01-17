df5469f31409878c0c9a2c44dd943a8a8da18b30
kajalp238 <kajalp@sahaj.ai>
2023-01-17 17:42:27 +0530

    Create order for buyer added

--- esop_trading/src/main/kotlin/com/example/Controller/CreateOrderController.kt
+++ esop_trading/src/main/kotlin/com/example/Controller/CreateOrderController.kt
@@ -64,9 +64,16 @@ class CreateOrderController {
 
             // add to buyer order history
             order.status = 'P'
-            if (order.quantity == sellOrder.quantity) {
+
+            if (order.quantity <= sellOrder.quantity) {
                 order.status = 'F'
+
+                if (order.quantity == sellOrder.quantity) {
+                    sellOrder.status = 'F'
+                }
             }
+
+
             var newOrder = order.copy()
             order.quantity = sellOrder.quantity
             if (orders[buyer.username] == null) {
@@ -146,55 +153,58 @@ class CreateOrderController {
 
 
         if(!buyqueue.isNotEmpty() || buyqueue.first().first < order.price) {
-            buyqueue.add(order.price to order)
+            sellqueue.add(order.price to order)
             return
         }
 
-        var sellOrder = sellqueue.poll().second
-        var seller = sellOrder.user
+        var buyOrder = buyqueue.poll().second
+        var buyer = buyOrder.user
 
-        // partial/full buy order case
-        if(order.quantity >= sellOrder.quantity){
+        // partial/full sell order case
+        if(order.quantity >= buyOrder.quantity){
 
             // add to seller order history
-            sellOrder.status = 'F'
+            buyOrder.status = 'F'
 
-            if (orders.get(sellOrder.user.username) == null) {
-                orders.put(sellOrder.user.username, arrayListOf())
+            if (orders.get(buyOrder.user.username) == null) {
+                orders.put(buyOrder.user.username, arrayListOf())
             }
 
-            orders[sellOrder.user.username]!!.add(sellOrder)
+            orders[buyOrder.user.username]!!.add(buyOrder)
 
             // add to buyer order history
             order.status = 'P'
-            if (order.quantity == sellOrder.quantity) {
+            if (order.quantity <= buyOrder.quantity) {
                 order.status = 'F'
+
+                if (order.quantity == buyOrder.quantity) {
+                    buyOrder.status = 'F'
+                }
             }
             var newOrder = order.copy()
-            order.quantity = sellOrder.quantity
-            if (orders[buyer.username] == null) {
-                orders[buyer.username] = arrayListOf()
+            order.quantity = buyOrder.quantity
+            if (orders[seller.username] == null) {
+                orders[seller.username] = arrayListOf()
             }
-            orders[buyer.username]?.add(order)
+            orders[seller.username]?.add(order)
 
 
-            newOrder.quantity -= sellOrder.quantity
+            newOrder.quantity -= buyOrder.quantity
 
             // inventory
-            seller.inventory.locked -= sellOrder.quantity
-            buyer.inventory.free += sellOrder.quantity
+            seller.inventory.locked -= buyOrder.quantity
+            buyer.inventory.free += buyOrder.quantity
 
             //wallet
-            var totalBoughtPrice = sellOrder.quantity * sellOrder.price
+            var totalBoughtPrice = buyOrder.quantity * order.price
             seller.wallet.free += totalBoughtPrice
             buyer.wallet.locked -= totalBoughtPrice
 
             newOrder.totalPrice -= totalBoughtPrice
 
-
-            addBuyOrder(newOrder)
+            addSellOrder(newOrder)
         } else {
-            // partial sell order
+            // partial buy order
 
             // add to seller order history
             order.status = 'F'
@@ -204,16 +214,16 @@ class CreateOrderController {
             orders[order.user.username]!!.add(order)
 
             // add to buyer order history
-            sellOrder.status = 'P'
+            buyOrder.status = 'P'
 
-            var newOrder = sellOrder.copy()
+            var newOrder = buyOrder.copy()
 
-            sellOrder.quantity = order.quantity
+            buyOrder.quantity = order.quantity
 
-            if (orders[seller.username] == null) {
-                orders[seller.username] = arrayListOf()
+            if (orders[buyer.username] == null) {
+                orders[buyer.username] = arrayListOf()
             }
-            orders[seller.username]?.add(sellOrder)
+            orders[buyer.username]?.add(buyOrder)
 
 
             newOrder.quantity -= order.quantity
@@ -224,14 +234,13 @@ class CreateOrderController {
             buyer.inventory.free += order.quantity
 
             //wallet
-            var totalBoughtPrice = sellOrder.quantity * sellOrder.price
+            var totalBoughtPrice = order.quantity * order.price
             seller.wallet.free += totalBoughtPrice
             buyer.wallet.locked -= totalBoughtPrice
 
             order.totalPrice -= totalBoughtPrice
 
-            sellqueue.add(sellOrder.price to newOrder)
-            addBuyOrder(order)
+            addBuyOrder(newOrder)
         }
     }
 
@@ -244,14 +253,16 @@ class CreateOrderController {
         var type = body.get("type").stringValue.trim()
         var price = body.get("price").intValue
 
-        var seller = User("ksjhdfkjs","sfd",37628,"sdjfhks", "sdhfs",
-            Inventory(0, 23), Wallet(0, 0))
-        var o1 = Order(1, 'U',  20, 5, 100, seller)
-        var o2 = Order(1, 'U', 3, 6, 18, seller)
-        sellqueue.add(5 to o1)
-        sellqueue.add(6 to o2)
-        UserController.users.put(seller.username, seller)
-        orders[seller.username] = arrayListOf()
+        var errors = ArrayList<String>()
+
+//        var seller = User("ksjhdfkjs","sfd",37628,"sdjfhks", "sdhfs",
+//            Inventory(0, 23), Wallet(0, 0))
+//        var o1 = Order(1, 'U',  20, 5, 100, seller)
+//        var o2 = Order(1, 'U', 3, 6, 18, seller)
+//        sellqueue.add(5 to o1)
+//        sellqueue.add(6 to o2)
+//        UserController.users.put(seller.username, seller)
+//        orders[seller.username] = arrayListOf()
 
         if(!UserController.users.containsKey(username)){
 
@@ -263,33 +274,31 @@ class CreateOrderController {
         var order = Order(orderId,'U', quantity, price, price*quantity, user)
         if(type=="BUY") {
             if (user.wallet.free < (order.totalPrice)) {
+                errors.add("insufficient wallet amount")
+            } else {
 
+                order.user.wallet.free -= (order.totalPrice)
+                order.user.wallet.locked += (order.totalPrice)
+
+                addBuyOrder(order)
             }
 
-            order.user.wallet.free -= (order.totalPrice)
-            order.user.wallet.locked += (order.totalPrice)
-
-            addBuyOrder(order)
         }
         else {
 
             if(user.inventory.free < quantity){
-                //ERRRROR
+                errors.add("insufficient ESOP quantity")
+            } else {
+                order.user.inventory.free -= (order.quantity)
+                order.user.inventory.locked += (order.quantity)
+
+                addSellOrder(order)
             }
-
-            order.user.inventory.free -= (order.quantity)
-            order.user.inventory.locked += (order.quantity)
-
-            addSellOrder(order)
         }
 
         var response = HashMap<String, Any>()
-        response["buyer"] = order.user
-        response["seller"] = seller
-
-        response["orderHistBuyer"] = orders[order.user.username]!!.toArray()
-
-        response["orderHistSell"] = orders[seller.username]!!.toArray()
+        response["errors"] = errors
+        response["orders"] = UserController.users.toMap()
 
         return response
     }
