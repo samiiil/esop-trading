45c55b3dcef1c36b1b08be56b1ff893202b53107
kajalp238 <kajalp@sahaj.ai>
2023-01-18 09:44:51 +0530

    Wallet refactored

---
+++ esop_trading/src/main/kotlin/Inventory.kt
@@ -0,0 +1,29 @@
+class Inventory {
+    private var freeInventory: Long=0L;
+    private var lockedInventory: Long=0L;
+
+    fun addEsopToInventory(esopsToBeAdded:Long){
+        this.freeInventory =  this.freeInventory + esopsToBeAdded;
+    }
+
+    fun getFreeInventory(): Long {
+        return this.freeInventory;
+    }
+
+    fun getLockedInventory(): Long {
+        return this.lockedInventory;
+    }
+
+    fun updateLockedInventory( inventoryToBeUpdated: Long){
+        this.lockedInventory = this.lockedInventory - inventoryToBeUpdated
+    }
+    fun moveFreeInventoryToLockedInventory(esopsToBeLocked:Long): String {
+        if(this.freeInventory >= esopsToBeLocked){
+            this.freeInventory = this.freeInventory - esopsToBeLocked;
+            this.lockedInventory = esopsToBeLocked;
+            return "Success";
+        }else{
+            return "Insufficient ESOPs in Inventory.";
+        }
+    }
+}
