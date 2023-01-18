1641181c6ed6fc03416a0a9dbce58478b231b8d8
kajalp238 <kajalp@sahaj.ai>
2023-01-18 09:39:34 +0530

    Code Refactoring

--- esop_trading/src/main/kotlin/com/example/Inventory.kt
+++
@@ -1,6 +0,0 @@
-package com.example
-
-data class Inventory(
-    var free:Int=0,
-    var locked:Int=0
-)
--- esop_trading/src/main/kotlin/com/example/Wallet.kt
+++
@@ -1,6 +0,0 @@
-package com.example
-
-data class Wallet(
-    var free:Int=0,
-    var locked:Int=0
-)
