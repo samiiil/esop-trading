9865bd20e0455c2436d088d52a9c2e65a1136ca0
sankar <sankaranarayananm@sahaj.ai>
2023-01-18 09:55:09 +0530

    refactor comparator for sell ESOPs

---
+++ esop_trading/src/main/kotlin/SellOrderingComparator.kt
@@ -0,0 +1,19 @@
+class SellOrderingComparator {
+    companion object : Comparator<Order>{
+        override fun compare(o1: Order, o2: Order): Int {
+            if (o1.orderPrice != o2.orderPrice){
+                if(o1.orderPrice > o2.orderPrice){
+                    return 1
+                }else{
+                    return -1
+                }
+            }else{
+                if (o1.orderId > o2.orderId){
+                    return 1
+                }else{
+                    return -1
+                }
+            }
+        }
+    }
+}
