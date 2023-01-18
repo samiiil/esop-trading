effc55bdc2b6acb2b2e0375932bdf098f4ab4684
Arun Murugan <arunmurugan@sahaj.ai>
2023-01-18 09:51:06 +0530

    refactor OrderExecutionLogs enitity

---
+++ esop_trading/src/main/kotlin/OrderExecutionLogs.kt
@@ -0,0 +1,11 @@
+import java.time.format.DateTimeFormatter;
+import java.time.ZoneOffset;
+import java.time.Instant;
+class OrderExecutionLogs ( val orderExecutionId: Long,
+                           val orderExecutionPrice: Long,
+                           val orderExecutionQuantity: Long){
+    val orderExecutionTime: String = DateTimeFormatter
+        .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
+        .withZone(ZoneOffset.UTC)
+        .format(Instant.now());
+}
