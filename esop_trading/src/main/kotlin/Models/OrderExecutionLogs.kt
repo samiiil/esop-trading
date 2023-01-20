f658a9148d80f45354a032a6e35c1e5a6f2f377b
MaheshAravindV <maheshv@sahaj.ai>
2023-01-21 00:37:47 +0530

    Refactor: Remove unnecessary space

--- esop_trading/src/main/kotlin/Models/OrderExecutionLogs.kt
+++ esop_trading/src/main/kotlin/Models/OrderExecutionLogs.kt
@@ -2,9 +2,9 @@ package Models
 import java.time.format.DateTimeFormatter;
 import java.time.ZoneOffset;
 import java.time.Instant;
-class OrderExecutionLogs ( val orderExecutionId: Long,
-                           val orderExecutionPrice: Long,
-                           val orderExecutionQuantity: Long){
+class OrderExecutionLogs (val orderExecutionId: Long,
+                          val orderExecutionPrice: Long,
+                          val orderExecutionQuantity: Long){
     val orderExecutionTime: String = DateTimeFormatter
         .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
         .withZone(ZoneOffset.UTC)
